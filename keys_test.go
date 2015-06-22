00bd2c780f071947e849a6da603f8dcd67718fe7
Vincent Rischmann <vincent@rischmann.fr>
2015-06-22 23:52:54 +0200

    Add another test for makeAllPossibleKeys

--- keys_test.go
+++ keys_test.go
@@ -25,6 +25,17 @@ func TestMakeAllPossibleKeys(t *testing.T) {
 	equals(t, "cassandra_ssl_cert", keys[2])
 	equals(t, "cassandrasslcert", keys[3])
 
+	fieldName = "Cassandra.SslCert"
+	keys = makeAllPossibleKeys(&context{
+		name: fieldName,
+	})
+
+	equals(t, 4, len(keys))
+	equals(t, "CASSANDRA_SSLCERT", keys[0])
+	equals(t, "CASSANDRA_SSL_CERT", keys[1])
+	equals(t, "cassandra_ssl_cert", keys[2])
+	equals(t, "cassandra_sslcert", keys[3])
+
 	fieldName = "Cassandra.SSLCert"
 	keys = makeAllPossibleKeys(&context{
 		name: fieldName,
